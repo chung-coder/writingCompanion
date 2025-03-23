46c8b7fdc50556ee6740fb7b92f6b2b1994c03f2
Yen Ting Chen <yen.tingchen@etudiant.univ-rennes.fr>
2025-03-23 23:54:49 +0100

    feat(admin): improve admin interface display and add null checks

--- backend/admin.py
+++ backend/admin.py
@@ -1,27 +1,30 @@
 from django.contrib import admin
-from backend.models import Class, Diary, Gptassistance, Gptinteraction, Student, Teacher
+from backend.models import Student, Teacher, Class, Diary, Gptassistance, Gptinteraction
 
 class ClassAdmin(admin.ModelAdmin):
     list_display = ('id','class_name')
     ordering = ('id',)
 
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user_id','name', 'gender', 'class_name', 'teacher_name')
+    list_display = ('id', 'user_id', 'name', 'gender', 'class_name', 'teacher_name')
     list_filter = ('gender',)
     ordering = ('id',)
+
     def user_id(self, obj):
-        return obj.user.id
+        return obj.user.id if obj.user else None
+
     def class_name(self, obj):
-        return obj.class_field.class_name
+        return obj.class_field.class_name if obj.class_field else "No Class"
+
     def teacher_name(self, obj):
-        return obj.teacher.name
+        return obj.teacher.name if obj.teacher else "No Teacher"
 
 class TeacherAdmin(admin.ModelAdmin):
     list_display = ('id','name')
     ordering = ('id',)
 
 class DiaryAdmin(admin.ModelAdmin):
-    list_display = ('_student', 'date', 'title', 'content', 'is_favorite', 'last_modified_date', 'mood', 'target', 'diary_type')
+    list_display = ('_student', 'date', 'title', 'content', 'mood', 'target', 'diary_type', 'is_favorite', 'last_modified_date')
     list_filter = ('is_favorite',)
     def _student(self, obj):
         return obj.student.name
